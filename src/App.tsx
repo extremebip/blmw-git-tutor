e2194c8f4af4485eef1a7bb5cef786e47401defc
extremebip <anthonygt124@gmail.com>
2024-10-17 14:45:37 +0700

    Add Jobs Page & JobListings conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,14 @@ import {
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
+import JobsPage from "./pages/JobsPage";
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
