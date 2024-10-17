14a5eacca5fd044464fea2f335bdb6650cf2d623
extremebip <anthonygt124@gmail.com>
2024-10-17 15:20:00 +0700

    Add & Scaffold React Toast

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/ReactToastify.min.css";
 
 const MainLayout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <ToastContainer />
     </>
   );
 };
