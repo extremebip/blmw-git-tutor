e9487212e23a66a80b566c8597251af43b32ae30
extremebip <anthonygt124@gmail.com>
2024-10-17 14:59:37 +0700

    Add Job Detail Page

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
 import JobsPage from "./pages/JobsPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
 
 const App = () => {
   const router = createBrowserRouter(
@@ -16,6 +17,7 @@ const App = () => {
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
